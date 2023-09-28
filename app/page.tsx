ff820c0c75bff04b975f973c18e6df57ff3a7c2b
taher1911 <72612843+taher1911@users.noreply.github.com>
2023-09-28 18:52:13 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,11 +26,8 @@ export default function Home() {
     const res = await fetch(
       "https://merri-bek.vic.gov.au/gis/AddressDetails.svc/fourbins/?xPoint=321300.65768899734&yPoint=5824000.948095851&wasteDay=Friday&wasteRateCode=101&recycleRateCode=142&fogoRateCode=142&glassRateCode=170&zone=B&glassWeekNumber=1&address=1B+UNDERA+COURT+FAWKNER+3060&cpage=87999",
       {
-        headers: {
-          Origin: "https://gathering-zeta.vercel.app/",
-          "Access-Control-Allow-Origin": "*",
-        },
-        mode: "cors", // no-cors, *cors, same-origin
+        
+        mode: "no-cors", // no-cors, *cors, same-origin
       }
     );
     const data = await res.json();
